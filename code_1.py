3e9d1469958d92413d2d6b550a582a56dc670d52
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-01-25 16:16:41 +0600

    second commit

--- code_1.py
+++ code_1.py
@@ -1,4 +1,8 @@
 print("Welcome to the world of Python progamming")
 name = "Farhan Akter Arnab"
 age = 15
+school = "Birshresththa Munshi Abdur Rouf Public College"
 print("My name is ", name, " and I am ", age, " years old")
+print("The name of my school is ", school)
+number = 3.14
+print("One of my favourite numbers is ", number)
