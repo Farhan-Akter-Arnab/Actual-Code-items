7bc0300cfba035466226e505993a1ec17c8e3b17
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-01-31 16:24:51 +0600

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+a = "Pythagoras"
+b = 75
+c = 5.00
+d = False
+bg = 'B'
+print("Type of a is ", type(a))
+print("Type of b is ", type(b))
+print("Type of c is ", type(c))
+print("Type of d is ", type(d))
+print("Type of bg is ", type(bg))
