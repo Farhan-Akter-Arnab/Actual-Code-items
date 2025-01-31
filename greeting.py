ee2da1f6fb49313707870a363a84fc7b34dab813
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-01-31 17:07:22 +0600

    initial commit

---
+++ greeting.py
@@ -0,0 +1,7 @@
+print("Welcome to our programme")
+name = input("Enter your name: ")
+print("Assalamu Alaikum, ", name, "! Nice to meet you.")
+condition = bool(input("Are you fine? Please answer in 'True' or 'False': "))
+print("You are fine. This statement is ", condition)
+print(type(name))
+print(type(condition))
