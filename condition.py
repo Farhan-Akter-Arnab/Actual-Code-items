0f698ba81d92c4afd3a4d51ea97e6ad7e8d9e92c
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-02-07 16:16:38 +0600

    initial commit

---
+++ condition.py
@@ -0,0 +1,7 @@
+number = float(input("Enter any number: "))
+if number<0:
+    print("Your given number is a negative number.")
+elif number>0:
+    print("Your given number is a positive number.")
+else:
+    print("Your number is nothing but a ZERO [0].")
