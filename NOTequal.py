1231e0f95f3712b1e49f43a39744e7dd9492c358
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-02-08 16:34:28 +0600

    initial commit

---
+++ NOTequal.py
@@ -0,0 +1,18 @@
+a = 3.14
+b = 8.24
+c = 8.24
+print(a != b)
+print(b != c)
+a = "Zero"
+b = "Null"
+if a != b:
+    print(a, " and ", b, " are different.")
+a = 10
+b = 11
+if (a==9) != (b==11):
+    print("Assalamu Alaikum! The code could run well")
+a = float(input("Enter any number: : "))
+if a%2 != 0:
+    print("Your number ", a, " is not an even number.")
+else:
+    print("Your number ", a, " is an even number.")
