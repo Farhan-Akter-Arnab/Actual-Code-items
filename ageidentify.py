b6bfd0fa17d267fbea62bdf24e82af58b15f0b30
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-02-14 17:13:47 +0600

    initial commit

---
+++ ageidentify.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age in numbers: "))
+if (age >= 10):
+    if (age <= 20):
+        print("You are allowed here")
+    else:
+        print("You are not allowed")
+else:
+    print("You are not allowed")
