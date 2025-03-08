4bb30c49e23b3567518c61e1be0689d9cd8766ef
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-03-08 16:01:43 +0600

    second commit

--- charoccur.py
+++ charoccur.py
@@ -6,4 +6,5 @@ while (i < len(string)):
     if (string[i] == char):
         count = count + 1
     i = i + 1
+print("Your word was = ", string)
 print("The total number of times ", char, " has occurred = ", count)
