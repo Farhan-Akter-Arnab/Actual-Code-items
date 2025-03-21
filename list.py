281eb3baa035f6bb18b60f1b0550722927d66871
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-03-21 16:19:10 +0600

    initial commit

---
+++ list.py
@@ -0,0 +1,10 @@
+# Create an empty list
+empty_list = []
+print(empty_list)
+numbers = [1,2,3,4,5]
+print(numbers)
+repetition = [1,2,3,4,5,6,7,8] * 3
+print(repetition)
+aList = [100,200,300,400,500]
+aList = aList[::-1]
+print(aList,"\n")
