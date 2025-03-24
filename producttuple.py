10a190848a286b2fe36744bcd5fb35b1c9862ab3
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-03-24 17:06:27 +0600

    modified commit

--- producttuple.py
+++ producttuple.py
@@ -3,3 +3,13 @@ product = 1
 for i in range(0,10):
     product*=tuplex[i]
 print("The product of numbers in the given tuple is = ", product)
+tuple1 = (4,3,2,2,-1,18)
+product = 1
+for i in range(0,6):
+    product*=tuplex[i]
+print("The product of numbers in the given tuple is = ", product)
+tuple2 = (2,4,8,8,3,2,9)
+product = 1
+for i in range(0,7):
+    product*=tuplex[i]
+print("The product of numbers in the given tuple is = ", product)
