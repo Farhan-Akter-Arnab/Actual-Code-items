4139d15f9eb0ed134575f8ffdb743a44bf8fe95c
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-05-03 17:33:51 +0600

    initial commit

---
+++ fruitquiz.py
@@ -0,0 +1,24 @@
+import random
+class FruitQuiz:
+    # Create a constructor
+    def __init__(self):
+        # Create a dictionary of fruits as keys and color as value
+        self.fruits = {'dates': 'brown',
+                       'grapes': 'green',
+                       'olive': 'green',
+                       'fig': 'purple'}
+    def ghora(self):
+        while (True):
+            fruit, color = random.choice(list(self.fruits.items()))
+            print("What is the color of ", fruit)
+            user_answer = input().lower()
+            if (user_answer == color):
+                print("Correct answer")
+            else:
+                print("Wrong answer")
+            option = int(input("Do you want to continue? 1 for yes, 0 for no:"))
+            if option == 0:
+                break
+print("Welcome to fruit quiz")
+fq = FruitQuiz()
+fq.ghora()
