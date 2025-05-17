fe07b17749ed79aae71f7a2ae5ccd8d8fc8ec600
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-05-17 17:11:43 +0600

    second commit

--- sprite_collision.py
+++ sprite_collision.py
@@ -2,11 +2,11 @@ import pygame
 import random
 SCREEN_WIDTH, SCREEN_HEIGHT = 824, 624
 MOVEMENT_SPEED = 6
-FONT_SIZE = 72.48
+FONT_SIZE = 72
 pygame.init()
-background_image = pygame.transform.scale(pygame.image.load("কান ধরে নিয়ে আসলাম রাতের আকাশ.jpg"), (SCREEN_WIDTH, SCREEN_HEIGHT))
+background_image = pygame.transform.scale(pygame.image.load("Milky Way.jpeg"), (SCREEN_WIDTH, SCREEN_HEIGHT))
 font = pygame.font.SysFont("Times New Roman", FONT_SIZE)
-class sprite:
+class Sprite(pygame.sprite.Sprite):
     def __init__(self, color, height, width):
         super().__init__()
         self.image = pygame.Surface([width, height])
@@ -35,16 +35,17 @@ while running:
             running = False
     if not won:
         keys = pygame.key.get_pressed()
-        x_change = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * MOVEMENT_SPEED
-        y_change = (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * MOVEMENT_SPEED
-        sprite1.move(x_change, y_change)
+        if keys[pygame.K_LEFT]: sprite1.rect.x-=3
+        if keys[pygame.K_RIGHT]: sprite1.rect.x+=3
+        if keys[pygame.K_UP]: sprite1.rect.y-=3
+        if keys[pygame.K_DOWN]: sprite1.rect.y+=3
         if sprite1.rect.colliderect(sprite2.rect):
             all_sprites.remove(sprite2)
             won = True
     screen.blit(background_image, (0,0))
     all_sprites.draw(screen)
     if won:
-        win_text = font.render("You Win!", True, pygame.color('white'))
+        win_text = font.render("You Win!", True, pygame.Color('white'))
         screen.blit(win_text, ((SCREEN_WIDTH - win_text.get_width()) // 2, (SCREEN_HEIGHT - win_text.get_height()) // 2))
     pygame.display.flip()
     clock.tick(124)
