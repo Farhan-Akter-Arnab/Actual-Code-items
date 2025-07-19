9275c3ed8442ded619f02246313c00d9f510e377
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-07-19 18:33:03 +0600

    initial commit

---
+++ O(1)_Time.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration = 0
+    print("The number entered by user is ", n)
+    iteration += 1
+    print("The number of iterations is ", iteration, "\n")
+printnumber(3 * 10**8)
+printnumber(6 * 10**24)
+print("\n With any 'n' the time taken by our code won't change.")
