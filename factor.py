b08745b9c3706c0119d4301f7e066555505825f4
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-08-01 18:40:12 +0600

    initial commit

---
+++ factor.py
@@ -0,0 +1,8 @@
+# Function to print the factors of a number
+def print_factors(n):
+    print("The factors of ", n, " are: ")
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(i)
+num = int(input("Enter your number to find its factors: "))
+print_factors(num)
