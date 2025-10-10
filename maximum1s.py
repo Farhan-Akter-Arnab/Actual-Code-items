54018d08d72438091c4a5d2745155ca599fa338d
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-10-10 09:43:03 +0600

    initial commit

---
+++ maximum1s.py
@@ -0,0 +1,24 @@
+
+def getmaxlength(a, a_size):
+    
+    counter = 0
+    
+    maxones = 0
+    
+    for i in range(0, a_size):
+    
+        if (a[i] == 0):
+    
+            counter = 0
+    
+        else:
+    
+            counter += 1
+            maxones = max(maxones, counter)
+    
+    return maxones
+
+a = [1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1]
+a_size = len(a)
+
+print("Maximum number of 1s = ", getmaxlength(a, a_size))
