752ee15bc5b22fa3703ba829c6ee41858fddf539
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2025-12-20 09:47:21 +0600

    347th commit

---
+++ slice.py
@@ -0,0 +1,24 @@
+# Input String
+inp1 = 'PARABOLA'
+inp2 = 'ARITHMETIC'
+inp3 = 'LARYNGOTRACHEOBRONCHITIS'
+# string slicing using indexing sequence
+print(inp1[:3])
+print(inp2[:3])
+print(inp3[:3])
+print(inp1[3:])
+print(inp2[3:])
+print(inp3[3:])
+print(inp1[1:5:2])
+print(inp2[1:5:2])
+print(inp3[1:5:2])
+# string slicing using negative indexing sequence
+print(inp1[::-1])
+print(inp2[::-1])
+print(inp3[::-1])
+print(inp1[-1:-9:-2])
+print(inp2[-1:-11:-2])
+print(inp3[-1:-25:-2])
+print(inp1[-6:5:2])
+print(inp2[-9:7:2])
+print(inp3[-22:21:2])
