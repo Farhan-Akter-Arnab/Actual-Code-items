92c67372e4bc4ccf5e9fed25550997d2452d3653
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2026-01-02 09:48:53 +0600

    358th commit

---
+++ lexicograph_succession.py
@@ -0,0 +1,24 @@
+def nextWord(s):
+    # If the string is empty, return "a"
+    if (s.split() == ""):
+        return "a"
+    
+    # Find the first character from the right
+    # which is not z
+    i = len(s) - 1
+    while (s[i] == 'z' and i>=0):
+        i -= 1
+    
+    # If all characters are z, then we need to
+    # add an extra character 'a' at the end
+    if (i == -1):
+        s = s + 'a'
+    # If there are some non-z characters
+    else:
+        s = s.replace(s[i], chr(ord(s[i]) + 1), 1)
+    
+    return s
+
+# Driver code
+inp = input("Enter a word: ")
+print("Next word is:", nextWord(inp))
