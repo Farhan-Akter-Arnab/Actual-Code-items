4469627c9eb45ff5d4a395338567d3b85956af33
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2026-01-10 09:52:06 +0600

    367th commit

---
+++ span_calculate.py
@@ -0,0 +1,24 @@
+# Python3 program to calculate span of stock
+def calculateSpan(price,S):
+    
+    n = len(price)
+    st = []
+    st.append(0)
+    S[0] = 1
+    
+    for i in range(1,n):
+        while (len(st) > 0 and price[st[-1]] <= price[i]):
+            st.pop()
+        S[i] = i + 1 if len(st) == 0 else (i - st[-1])
+        st.append(i)
+        
+def printArray(arr, n):
+    for i in range(n):
+        print(arr[i], end = " ")
+        
+price = [10, 4, 5, 90, 120, 80]
+S = [0 for i in range(len(price))]
+print(S)
+
+calculateSpan(price, S)
+printArray(S, len(price))
