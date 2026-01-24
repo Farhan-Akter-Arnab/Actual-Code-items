24f76cc0b333a8d570a29bb16ee1ee91050d4bc2
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2026-01-24 09:57:23 +0600

    373rd commit

---
+++ queue_doublyll.py
@@ -0,0 +1,44 @@
+class Oghuz_Khan:
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.prev = None
+            self.next = None
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.Size = 0
+    def isEmpty(self):
+        return self.front is None
+    def size(self):
+        return self.Size
+    def insertFront(self, data):
+        newNode = Oghuz_Khan.Node(data)
+        if self.front is None():
+            self.front = self.rear = newNode
+        else:
+            newNode.next = self.front
+            self.front.prev = newNode
+            self.front = newNode
+        self.Size += 1
+    def insertRear(self, data):
+        newNode = Oghuz_Khan.Node(data)
+        if self.rear is None:
+            self.front = self.rear = newNode
+        else:
+            newNode.prev = self.rear
+            self.rear.next = newNode
+            self.rear = newNode
+        self.Size += 1
+    def deleteFront(self):
+        if self.isEmpty():
+            print("Dequeue is Empty")
+            return
+        temp = self.front
+        if self.front == self.rear:
+            self.front = self.rear = None
+        else:
+            self.front = self.front.next
+            self.front.prev = None
+        self.Size -= 1
+        return data
