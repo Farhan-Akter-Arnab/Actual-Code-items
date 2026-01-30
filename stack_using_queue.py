b6f77db4f1a878557aa5f3f2d180854e2521f699
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2026-01-30 17:55:56 +0600

    375th commit

---
+++ stack_using_queue.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Stack:
+    def __init__(self):
+        self.q1 = deque(); self.q2 = deque()
+    def push(self, x):
+        self.q2.append(x)
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+            print("q2 after moving elements from q1:", list(self.q2))
+        self.q1, self.q2 = self.q2, self.q1
+    def pop(self):
+        if self.q1: self.q1.popleft()
+    def top(self):
+        if self.q1: return self.q1[0]
+        return None
+    def size(self):
+        return len(self.q1)
+if __name__ == '__main__':
+    s = Stack()
+    s.push(1); s.push(2); s.push(3); s.push(4)
+    print("Current size:", s.size())
+    print(s.top()); s.pop(); print(s.top())
+    s.pop(); print(s.top());
+    print("Current size: ", s.size())
