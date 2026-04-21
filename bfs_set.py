018a6f8e9653b880cc134d293be8172293bb1256
Farhan-Akter-Arnab <162998arnab101m04ev@gmail.com>
2026-04-21 21:12:55 +0600

    417th commit

---
+++ bfs_set.py
@@ -0,0 +1,24 @@
+from collections import deque
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=" ")
+
+        for neighbour in graph[vertex]:
+            if neighbour not in visited:
+                queue.append(neighbour)
+                visited.add(neighbour)
+# Example usage:
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+print("Breadth-First Search (BFS) Traversal: ")
+bfs(graph, 'A')
